50169be014ad5b908b59ee35074fd6bb8a9e78d5
Sasa33 <chengsasa33@gmail.com>
2017-06-16 09:27:40 +0800

    change to use hashRouter so that github pages can work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import App from './containers/App';
 import Cartoons from './containers/Cartoons';
 import About from './containers/About';
@@ -11,13 +11,13 @@ import './index.css';
 
 ReactDOM.render((
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route path="/cartoons" component={Cartoons} />
                 <Route path="/about" component={About} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
