99fc72134089402cab503a4a8476b5392d74a894
Sasa33 <chengsasa33@gmail.com>
2017-06-23 17:52:06 +0800

    use table to put the search results

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,27 +2,31 @@ import React from 'react'
 import './style.css'
 
 const Table = ({ list, loadMore, nextPage }) => (
-    <div className="Table">
-        <div className="Table-header">
-            <span>Title</span>
-            <span>Author</span>
-            <span>Num of Comments</span>
-            <span>Points</span>
-        </div>
-        { list.map(item =>
-            <div key={item.objectID} className="Table-item">
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-            </div>
-        )}
+    <table className="Search-result">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Num of Comments</th>
+                <th>Points</th>
+            </tr>
+        </thead>
+        <tbody>
+            { list.map(item =>
+                <tr key={item.objectID}>
+                    <td>
+                      <a href={item.url}>{item.title}</a>
+                    </td>
+                    <td>{item.author}</td>
+                    <td>{item.num_comments}</td>
+                    <td>{item.points}</td>
+                </tr>
+            )}
+        </tbody>
         <button onClick={() => loadMore(nextPage)}>
             Load More
         </button>
-    </div>
+    </table>
 );
 
 export default Table
